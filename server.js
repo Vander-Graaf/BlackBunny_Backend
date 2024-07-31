caa6dc0c93eebed5155d2fe4043ffb1754212082
Vander-Graaf <138441872+Vander-Graaf@users.noreply.github.com>
2024-08-01 05:27:14 +0600

    deploy

--- server.js
+++ server.js
@@ -5,12 +5,25 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
-const port = 5000;
 
-app.use(cors());
+const port = 5000;
+const allowedOrigins = ["https://blackbunny.onrender.com"];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 app.use(express.json());
 
 const uri = "mongodb+srv://Vander:Qq9JsBUOi7WKeM72@store.z3zyqyw.mongodb.net/";
+
 mongoose
   .connect(uri)
   .then(() => {
