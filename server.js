a5a2a0af03b643f627f27bae5b076f5e3e0e6e53
Vander-Graaf <138441872+Vander-Graaf@users.noreply.github.com>
2024-08-02 02:54:12 +0600

    deploy

--- server.js
+++ server.js
@@ -28,13 +28,13 @@ const usersRouter = require("./routes/users");
 app.use("/products", productsRouter);
 app.use("/users", usersRouter);
 
-if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
-  app.use(express.static(path.join(__dirname, "client", "build"))); // Use 'path.join' for better cross-platform compatibility
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
+//  app.use(express.static(path.join(__dirname, "client", "build"))); // Use 'path.join' for better cross-platform compatibility
+//
+//  app.get("*", (req, res) => {
+//    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//  });
+//}
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
